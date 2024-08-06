4eb438ed6b7e7d13c6159233147217880d2d48a1
ogbozoyan <oganes.bozoyan@mail.ru>
2024-08-06 22:22:11 +0300

    feature: changed pmt calculation logic

--- src/App.js
+++ src/App.js
@@ -8,10 +8,8 @@ function App() {
     principal: 0.0,
     paymentBefore: 0.0,
     interestRate: 0.0,
-    loanTermMonths: 0,
     deffermentMonthCount: 0,
-    deffermentFromMonth: '',
-    loanStartDate: '',
+    leftMonthCount: 0,
     isOnlyInterest: false
   });
 
@@ -25,33 +23,15 @@ function App() {
     });
   };
 
-  const calculateDeffermentFromMonth = () => {
-    const startDate = new Date(values.loanStartDate);
-    const deffermentDate = new Date(values.deffermentFromMonth);
-
-    const startYear = startDate.getFullYear();
-    const startMonth = startDate.getMonth();
-    const deffermentYear = deffermentDate.getFullYear();
-    const deffermentMonth = deffermentDate.getMonth();
-
-    const monthDiff = (deffermentYear - startYear) * 12 + (deffermentMonth - startMonth);
-
-    return monthDiff + 1; // +1, так как месяцы начинаются с 0
-  };
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    const paymentMonthCount = parseInt(values.loanTermMonths, 10);
-    const deffermentFromMonthNumber = calculateDeffermentFromMonth();
 
     setSubmittedValues({
       principal: parseFloat(values.principal),
       paymentBefore: parseFloat(values.paymentBefore),
       interestRate: parseFloat(values.interestRate),
-      paymentMonthCount,
       deffermentMonthCount: parseInt(values.deffermentMonthCount, 10),
-      deffermentFromMonth: deffermentFromMonthNumber,
-      loanStartDate: values.loanStartDate,
+      leftMonthCount: parseInt(values.leftMonthCount, 10),
       isOnlyInterest: values.isOnlyInterest
     });
   };
@@ -64,15 +44,13 @@ function App() {
           principal={submittedValues.principal}
           interestRate={submittedValues.interestRate}
           paymentBefore={submittedValues.paymentBefore}
-          paymentMonthCount={submittedValues.paymentMonthCount}
           deffermentMonthCount={submittedValues.deffermentMonthCount}
-          deffermentFromMonth={submittedValues.deffermentFromMonth}
-          loanStartDate={submittedValues.loanStartDate}
+          leftMonthCount={submittedValues.leftMonthCount}
           isOnlyInterest={submittedValues.isOnlyInterest}
         />
       )}
     </div>
-  );
+  ); 
 }
 
 export default App;
